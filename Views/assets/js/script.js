ccbe2a23641ac3bbb7c7cba1e8c2ec7110196c86
Gauthier GLAD.CHAMBET <gauthier.glad.chambet@labom2iformation.fr>
2026-03-23 14:46:22 +0100

    Style de la page d'inscription/connexion terminé0

--- Views/assets/js/script.js
+++ Views/assets/js/script.js
@@ -2,7 +2,16 @@ const POPUP_SUPPR = document.querySelectorAll(".popupSuppr");
 const BTN_SUPPR = document.querySelectorAll(".bouttonSupprimer");
 const BTN_ANNULER_SUPPR = document.querySelectorAll(".annulerSuppr");
 const CONFIRMPWD = document.getElementById("confirmPassword");
+const BTN_INSCRIPTION = document.getElementById("button-inscription");
+const BTN_CONNECTION = document.getElementById("button-connection");
+const LABEL_INSCRIPTION = document.getElementById("label-inscription");
+const LABEL_CONNECTION = document.getElementById("label-connection");
+const BLOCK_CONNECTION = document.getElementById("block-connection");
+const BLOCK_INSCRIPTION = document.getElementById("block-inscription");
 
+
+
+// Ouvre la popup de confirmation de suppression
 BTN_SUPPR.forEach(btn => {
     btn.addEventListener("click", () => {
         POPUP_SUPPR.forEach(btn =>{
@@ -11,6 +20,7 @@ BTN_SUPPR.forEach(btn => {
     });
 });
 
+// Ferme la popup de confirmation de suppresion
 BTN_ANNULER_SUPPR.forEach(btn => {
     btn.addEventListener("click", () => {
         POPUP_SUPPR.forEach(btn =>{
@@ -20,3 +30,42 @@ BTN_ANNULER_SUPPR.forEach(btn => {
     });
 });
 
+//Switch l'onglet Inscription/Connexion
+BTN_INSCRIPTION.addEventListener("click", () => {
+    BLOCK_INSCRIPTION.style.display = "flex";
+    BLOCK_CONNECTION.style.display = "none";
+
+
+    BTN_CONNECTION.classList.add("color-green");
+    LABEL_CONNECTION.classList.add("color-green");
+    BTN_CONNECTION.classList.remove("color-grey");
+    LABEL_CONNECTION.classList.remove("color-grey");
+
+    BTN_INSCRIPTION.classList.add("color-grey");
+    LABEL_INSCRIPTION.classList.add("color-grey");
+    BTN_INSCRIPTION.classList.remove("color-green");
+    LABEL_INSCRIPTION.classList.remove("color-green");
+
+    BTN_CONNECTION.style.zIndex = "0";
+    BTN_INSCRIPTION.style.zIndex = "2";
+})
+
+BTN_CONNECTION.addEventListener("click", () => {
+    BLOCK_CONNECTION.style.display = "flex";
+    BLOCK_INSCRIPTION.style.display = "none";
+
+
+    BTN_INSCRIPTION.classList.add("color-green");
+    LABEL_INSCRIPTION.classList.add("color-green");
+    BTN_INSCRIPTION.classList.remove("color-grey");
+    LABEL_INSCRIPTION.classList.remove("color-grey");
+
+
+    BTN_CONNECTION.classList.add("color-grey");
+    LABEL_CONNECTION.classList.add("color-grey");
+    BTN_CONNECTION.classList.remove("color-green");
+    LABEL_CONNECTION.classList.remove("color-green");
+
+    BTN_INSCRIPTION.style.zIndex = "0";
+    BTN_CONNECTION.style.zIndex = "2";
+})
--- Views/user/signIn.php
+++ Views/user/signIn.php
@@ -3,60 +3,74 @@
         <p class="messageSuccess"><?= $_SESSION['success']['CompteSupprime'] ?></p>
         <?php unset($_SESSION['success']['CompteSupprime']); ?>
     <?php }; ?>
+    <?php if (!empty($_SESSION['error'])){ 
+                            foreach($_SESSION['error'] as $message) {
+                        ?>
+                            <p class="messageError"><?= $message ?></p>
+                        <?php }
+                            unset($_SESSION['error']);
+                        } ?>
+                    
+                        <?php if (!empty($_SESSION['success']['utilisateurAjoute'])) { ?>
+                            <p class="messageSuccess"><?= $_SESSION['success']['utilisateurAjoute'] ?></p>
+                            <?php unset($_SESSION['success']['utilisateurAjoute']); ?>
+                        <?php }; ?>
+    <div class="logo-signIn">
+        <img src="/BoardCompanion/Views/assets/img/logo.png" alt="Logo de Board Companion">
+        <h1>Board Companion</h1>
+        <p>Ton assistant boarder personnel</p>
+    </div>
+
     <div class="container-signIn">
-        <div class="logo logo-signIn">
-            <img src="/BoardCompanion/Views/assets/img/logo.png" alt="Logo de Board Companion">
-            <h1>Board Companion</h1>
-            <p>Ton assistant boarder personnel</p>
+        <div class="login-onglets">
+            <div class="container login-button" id="button-inscription">
+                <h2 id="label-inscription">Inscription</h2>
+            </div>
+            <div class="container login-button color-green" id="button-connection">
+                <h2 class="color-green" id="label-connection">Connexion</h2>
+            </div>
         </div>
-        <div class="container">
-            <form action="" method="POST">
-                <div>
-                    <label for="email">Email :</label>
-                    <input type="text" name="email" id="email" required>
-                </div>
-                <div>
-                    <label for="pseudo ">Prénom ou Pseudo :</label>
-                    <input type="text" name="pseudo" id="pseudo" required>
-                </div>
-                <div>
-                    <label for="password">Mot de passe :</label>
-                    <input type="password" name="password" id="password" required>
-                </div>
-                <div>
-                    <label for="passwordConfirmation">Confirmez le mot de passe :</label>
-                    <input type="password" name="passwordConfirmation" id="passwordConfirmation" required>
-                </div>
-                <div>
-                    <label for="accepteCGU"><input type="checkbox" id="accepteCGU" name="accepteCGU">J'accepte les termes et conditions d'utilisation</label>
-                </div>
-                <input class="button" type="submit" name="submit_signUp" id="submit_signUp" value="S'inscrire">
+
+        <div class="container block-login" id="block-connection">
+                <form action="" method="POST">
+                    <div>
+                        <label for="email2">Email :</label>
+                        <input type="text" name="email" id="email2" autocomplete="on" required>
+                    </div>
+                    <div>
+                        <label for="password2">Mot de passe :</label>
+                        <input type="password" name="password" id="password2" required>
+                    </div>
+                        <input class="button" type="submit" name="submit_signIn" id="submit_signIn" value="Se connecter">
                 </form>
-                    <?php if (!empty($_SESSION['error'])){ 
-                        foreach($_SESSION['error'] as $message) {
-                    ?>
-                        <p class="messageError"><?= $message ?></p>
-                    <?php }
-                        unset($_SESSION['error']);
-                    } ?>
-                
-                    <?php if (!empty($_SESSION['success']['utilisateurAjoute'])) { ?>
-                        <p class="messageSuccess"><?= $_SESSION['success']['utilisateurAjoute'] ?></p>
-                        <?php unset($_SESSION['success']['utilisateurAjoute']); ?>
-                    <?php }; ?>
         </div>
-        <div class="container">
+
+        <div class="container block-login" id="block-inscription">
                 <form action="" method="POST">
                     <div>
                         <label for="email">Email :</label>
-                        <input type="text" name="email" id="email" required>
+                        <input type="text" name="email" id="email" autocomplete="on" required>
+                    </div>
+                    <div>
+                        <label for="pseudo">Prénom ou Pseudo :</label>
+                        <input type="text" name="pseudo" id="pseudo" required>
                     </div>
                     <div>
                         <label for="password">Mot de passe :</label>
                         <input type="password" name="password" id="password" required>
                     </div>
-                        <input class="button" type="submit" name="submit_signIn" id="submit_signIn" value="Se connecter">
-                </form>
-        </div>
-    </div>
+                    <div>
+                        <label for="passwordConfirmation">Confirmez le mot de passe :</label>
+                        <input type="password" name="passwordConfirmation" id="passwordConfirmation" required>
+                    </div>
+                    <div>
+                        <label class="checkboxLabel" for="accepteCGU">J'accepte les termes et conditions d'utilisation
+                            <input type="checkbox" id="accepteCGU" name="accepteCGU" required>
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+                    <input class="button" type="submit" name="submit_signUp" id="submit_signUp" value="S'inscrire">
+                    </form>
+                </div>
+        </div>                
 </main>
