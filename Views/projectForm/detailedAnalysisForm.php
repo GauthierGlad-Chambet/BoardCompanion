09c2698cbfe3e1d32b974badb5067d1a01a6f3c7
Gauthier GLAD.CHAMBET <gauthier.glad.chambet@labom2iformation.fr>
2026-03-25 17:25:20 +0100

    Style destkop de page analyse détaillée terminé

--- Views/projectForm/detailedAnalysisForm.php
+++ Views/projectForm/detailedAnalysisForm.php
@@ -1,77 +1,70 @@
 <main>
-
-<h1 id="analyse-detaillee">Analyse detaillee</h1>
-
-<h2 id="h2-analyse-detaillee">
-<?php echo "{$project->getName()}, {$project->getStudio()}, Épisode n<sup>o</sup> {$project->getEpisode_nb()} : {$project->getEpisode_title()}"?>
-</h2>
-
-<div class="container" >
-    <?php if (!empty($sequenceHeaders)): ?>
-    <form action="" method="POST">
-        <div class="all-sequences-container">
-
-            <!-- Champ caché pour transmettre l'ID du projet -->
-            <input type="hidden" name="project_id" value="<?= htmlspecialchars($projectId) ?>">
-    
-            <?php  foreach ($sequenceHeaders as $index => $sequence): ?>
-            <!-- Champ caché pour transmettre le titre de la séquence -->
-            <input type="hidden" name="sequence_header_<?= $index ?>" value="<?= htmlspecialchars($sequence['header']) ?>">
-                <div class="sequence-block">
+    <h1 id="analyse-detaillee">Analyse detaillee</h1>
+    <h2 id="h2-analyse-detaillee">
+    <?php echo "{$project->getName()}, {$project->getStudio()}, Épisode n<sup>o</sup> {$project->getEpisode_nb()} : {$project->getEpisode_title()}"?>
+    </h2>
+    <div class="container" id="container-detailed-analysis">
+        <?php if (!empty($sequenceHeaders)): ?>
+            <form action="" method="POST">
+                <div class="all-sequences-container">
+                    <!-- Champ caché pour transmettre l'ID du projet -->
+                    <input type="hidden" name="project_id" value="<?= htmlspecialchars($projectId) ?>">
+                    <?php  foreach ($sequenceHeaders as $index => $sequence): ?>
+                    <!-- Champ caché pour transmettre le titre de la séquence -->
+                    <input type="hidden" name="sequence_header_<?= $index ?>" value="<?= htmlspecialchars($sequence['header']) ?>">
                     <h3>Séquence <?= $index + 1 ?> : </br> <?= htmlspecialchars($sequence['header']) ?></h3>
                     <p class="note"><?= $sequence['line_count'] ?> lignes</p>
-                    <div class="containerSequence">
-                        <ul>
-                            <!-- Affichage des 5 premières lignes de la séquence pour un aperçu -->
-                            <?php foreach (array_slice($sequence['content'], 0, 5) as $line): ?>
-                                <li><?= htmlspecialchars($line) ?></li>
-                                <?php endforeach; ?>
-                                <li>…</li>
-                            </ul>
-                        </div>
-    
-                    <!-- Champ caché pour transmettre le contenu de la séquence -->
-                    <input type="hidden" name="sequence_content_<?= $index ?>" value="<?= htmlspecialchars(json_encode($sequence['content'])) ?>">
-                </div>
-                <div class="container-sequence-radio">
                     <div>
-                        <p>Type de séquence :</p>
-                        <label class="customLabel radioLabel" for="SequenceAction_<?= $index ?>">Action
-                            <input type="radio" id="SequenceAction_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Action" checked />
-                            <span class="checkmark"></span>     
-                        </label>
-                        <label class="customLabel radioLabel" for="SequenceComedie_<?= $index ?>">Comédie
-                            <input type="radio" id="SequenceComedie_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Comedie" />
-                            <span class="checkmark"></span>
-                        </label>
-                        <label class="customLabel radioLabel" for="SequenceMixte_<?= $index ?>">Mixte
-                            <input type="radio" id="SequenceMixte_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Mixte" />
-                            <span class="checkmark"></span>
-                        </label>
-                    </div>
-                    <div>
-                        <p>Je m'en occupe :</p>
-                        <div class="container-occupe">
-                            <label class="customLabel radioLabel" for="assigned_<?= $index ?>">Oui
-                                <input type="radio" id="assigned_<?= $index ?>" name="is_assigned_<?= $index ?>" value="1" checked />
-                                <span class="checkmark"></span>
-                            </label>
-                            <label class="customLabel radioLabel" for="not_assigned_<?= $index ?>">Non
-                                <input type="radio" id="not_assigned_<?= $index ?>" name="is_assigned_<?= $index ?>" value="0" />
-                                <span class="checkmark"></span>
-                            </label>
+                        <div class="container-sequence">
+                            <ul>
+                                <!-- Affichage des 5 premières lignes de la séquence pour un aperçu -->
+                                <?php foreach (array_slice($sequence['content'], 0, 5) as $line): ?>
+                                    <li><?= htmlspecialchars($line) ?></li>
+                                    <?php endforeach; ?>
+                                    <li>…</li>
+                                </ul>
                         </div>
-                        
+                        <!-- Champ caché pour transmettre le contenu de la séquence -->
+                        <input type="hidden" name="sequence_content_<?= $index ?>" value="<?= htmlspecialchars(json_encode($sequence['content'])) ?>">
+
+                        <div class="container-sequence-radio">
+                            <div>
+                                <p>Type de séquence :</p>
+                                <label class="customLabel radioLabel" for="SequenceAction_<?= $index ?>">Action
+                                    <input type="radio" id="SequenceAction_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Action" checked />
+                                    <span class="checkmark"></span>     
+                                </label>
+                                <label class="customLabel radioLabel" for="SequenceComedie_<?= $index ?>">Comédie
+                                    <input type="radio" id="SequenceComedie_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Comedie" />
+                                    <span class="checkmark"></span>
+                                </label>
+                                <label class="customLabel radioLabel" for="SequenceMixte_<?= $index ?>">Mixte
+                                    <input type="radio" id="SequenceMixte_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Mixte" />
+                                    <span class="checkmark"></span>
+                                </label>
+                            </div>
+                            <div>
+                                <p>Je m'en occupe :</p>
+                                <div class="container-occupe">
+                                    <label class="customLabel radioLabel" for="assigned_<?= $index ?>">Oui
+                                        <input type="radio" id="assigned_<?= $index ?>" name="is_assigned_<?= $index ?>" value="1" checked />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <label class="customLabel radioLabel" for="not_assigned_<?= $index ?>">Non
+                                        <input type="radio" id="not_assigned_<?= $index ?>" name="is_assigned_<?= $index ?>" value="0" />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    <hr>
+                    <?php endforeach; ?>
                 </div>
-                    
-                <hr>
-            <?php endforeach; ?>
-        </div>
-        <input class="button" type="submit" name="submit_sequences" id="submit_sequences" value="Valider les séquences">
-    </form>
-    <?php else: ?>
-    <p>Aucune scène trouvée.</p>
-<?php endif; ?>
-</div>
+                <input class="button" type="submit" name="submit_sequences" id="submit_sequences" value="Valider les séquences">
+            </form>
+        <?php else: ?>
+        <p>Aucune scène trouvée.</p>
+        <?php endif; ?>
+    </div>
 </main>
--- Views/projectForm/updateDetailedAnalysisForm.php
+++ Views/projectForm/updateDetailedAnalysisForm.php
@@ -1,17 +1,13 @@
 <main>
-
-<h1>Modifier l'analyse detaillee</h1>
-
-<form class="form-select" action="index.php" method="get">
-
+    <h1>Modifier l'analyse detaillee</h1>
+    <form class="form-select" action="index.php" method="get">
         <!-- Conserver les paramètres controller et action -->
         <input type="hidden" name="controller" value="form">
-        <?php if ($project->getIs_detailed() == 0 ) { ?>
+        <?php if ($project->getIs_detailed() == 0) { ?>
             <input type="hidden" name="action" value="detailedAnalysis">
         <?php } else { ?>
             <input type="hidden" name="action" value="updateDetailedAnalysis">
         <?php } ?>
-
         <select name="project_id" id="">
             <?php foreach ($projects as $proj) : ?>
                 <option value="<?= $proj->getId() ?>" <?= ($proj->getId() == $project->getId()) ? 'selected' : '' ?>>
@@ -19,71 +15,66 @@
                 </option> <?php endforeach; ?>
         </select>
         <!-- <input class="button" type="submit" value="Changer de projet"> -->
-</form>
-
-<div class="container" >
-<?php if (!empty($sequences)): ?>
-    
-    <form action="" method="POST">
-        <div class="all-sequences-container">
-
-            <input type="hidden" name="project_id" value="<?= htmlspecialchars($projectId) ?>">
-            <?php foreach ($sequences as $index => $sequence): ?>
-    
-                <!-- Champ caché pour identifier la séquence à mettre à jour -->
-                <input type="hidden" name="sequence_id_<?= $index ?>" value="<?= $sequence->getId() ?>">
-           
-                <div class="sequence-block">
-                    <h3>Séquence <?= $sequence->getNumber() ?> : </br> <?= htmlspecialchars($sequence->getTitle()) ?></h3>
-                    <p class="note"><?= $sequence->getLines_count() ?> lignes</p>
-                    <div class="containerSequence">
-                        <ul>
-                            <?php foreach (array_slice(json_decode($sequence->getScript(), true) ?? [], 0, 5) as $line): ?>
-                                <li><?= htmlspecialchars($line) ?></li>
-                            <?php endforeach; ?>
-                            <li>…</li>
-                        </ul>
-                    </div>
-                </div>
-    
-                <div class="container-sequence-radio">
-                    <div>
-                        <p>Type de séquence :</p>
-                        <label class="customLabel radioLabel" for="SequenceAction_<?= $index ?>">Action
-                            <input type="radio" id="SequenceAction_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Action" <?= $sequence->getFk_type() == 1 ? 'checked' : '' ?> />
-                            <span class="checkmark"></span>
-                        </label>
-                        <label class="customLabel radioLabel" for="SequenceComedie_<?= $index ?>">Comédie
-                            <input type="radio" id="SequenceComedie_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Comedie" <?= $sequence->getFk_type() == 2 ? 'checked' : '' ?> />
-                            <span class="checkmark"></span>
-                        </label>
-                        <label class="customLabel radioLabel" for="SequenceMixte_<?= $index ?>">Mixte
-                            <input type="radio" id="SequenceMixte_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Mixte" <?= $sequence->getFk_type() == 3 ? 'checked' : '' ?> />
-                            <span class="checkmark"></span>
-                        </label>
-                    </div>
-                    <div>
-                        <p>Je m'en occupe :</p>
-                        <div class="container-occupe">
-                            <label class="customLabel radioLabel" for="assigned_<?= $index ?>">Oui
-                                <input type="radio" id="assigned_<?= $index ?>" name="is_assigned_<?= $index ?>" value="1" <?= $sequence->getIs_assigned() ? 'checked' : '' ?> />
-                                <span class="checkmark"></span>
-                            </label>
-                            <label class="customLabel radioLabel" for="not_assigned_<?= $index ?>">Non
-                                <input type="radio" id="not_assigned_<?= $index ?>" name="is_assigned_<?= $index ?>" value="0" <?= !$sequence->getIs_assigned() ? 'checked' : '' ?> />
-                                <span class="checkmark"></span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-                <hr>
-            <?php endforeach; ?>
-        </div>
-
-        <input class="button" type="submit" name="submit_sequences" id="submit_sequences" value="Valider les séquences">
     </form>
-<?php else: ?>
-    <p>Aucune scène trouvée.</p>
-<?php endif; ?>
-</div>
+    <div class="container" id="container-detailed-analysis">
+        <?php if (!empty($sequences)): ?>
+            <form action="" method="POST">
+                <div class="all-sequences-container">
+                    <input type="hidden" name="project_id" value="<?= htmlspecialchars($projectId) ?>">
+                    <?php foreach ($sequences as $index => $sequence): ?>
+
+                        <!-- Champ caché pour identifier la séquence à mettre à jour -->
+                        <input type="hidden" name="sequence_id_<?= $index ?>" value="<?= $sequence->getId() ?>">
+                        <h3>Séquence <?= $sequence->getNumber() ?> : </br> <?= htmlspecialchars($sequence->getTitle()) ?></h3>
+                        <p class="note"><?= $sequence->getLines_count() ?> lignes</p>
+                        <div>
+                            <div class="container-sequence">
+                                <ul>
+                                    <?php foreach (array_slice(json_decode($sequence->getScript(), true) ?? [], 0, 5) as $line): ?>
+                                        <li><?= htmlspecialchars($line) ?></li>
+                                    <?php endforeach; ?>
+                                    <li>…</li>
+                                </ul>
+                            </div>
+    
+                            <div class="container-sequence-radio">
+                                <div>
+                                    <p>Type de séquence :</p>
+                                    <label class="customLabel radioLabel" for="SequenceAction_<?= $index ?>">Action
+                                        <input type="radio" id="SequenceAction_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Action" <?= $sequence->getFk_type() == 1 ? 'checked' : '' ?> />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <label class="customLabel radioLabel" for="SequenceComedie_<?= $index ?>">Comédie
+                                        <input type="radio" id="SequenceComedie_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Comedie" <?= $sequence->getFk_type() == 2 ? 'checked' : '' ?> />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <label class="customLabel radioLabel" for="SequenceMixte_<?= $index ?>">Mixte
+                                        <input type="radio" id="SequenceMixte_<?= $index ?>" name="typeSequence_<?= $index ?>" value="Mixte" <?= $sequence->getFk_type() == 3 ? 'checked' : '' ?> />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                                <div>
+                                    <p>Je m'en occupe :</p>
+                                    <div class="container-occupe">
+                                        <label class="customLabel radioLabel" for="assigned_<?= $index ?>">Oui
+                                            <input type="radio" id="assigned_<?= $index ?>" name="is_assigned_<?= $index ?>" value="1" <?= $sequence->getIs_assigned() ? 'checked' : '' ?> />
+                                            <span class="checkmark"></span>
+                                        </label>
+                                        <label class="customLabel radioLabel" for="not_assigned_<?= $index ?>">Non
+                                            <input type="radio" id="not_assigned_<?= $index ?>" name="is_assigned_<?= $index ?>" value="0" <?= !$sequence->getIs_assigned() ? 'checked' : '' ?> />
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <?php endforeach; ?>
+                    </div>
+            <input class="button" type="submit" name="submit_sequences" id="submit_sequences" value="Valider les séquences">
+            </form>
+        <?php else: ?>
+            <p>Aucune scène trouvée.</p>
+        <?php endif; ?>
+    </div>
 </main>
